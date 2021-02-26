7a8a8ac5903cebe19870f5290377600a36bd4a5c
VadimMorev <77121381+VadimMorev@users.noreply.github.com>
2021-02-26 16:20:48 +0200

    Changed Weather class, add deserialization from JSON-string to object.

--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -1,6 +1,10 @@
+import org.json.JSONObject;
+
 import java.io.*;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
 public class Weather {
     private static final String API_KEY="d5ddc3bdb813a23fe8fa89edbac1cca7";
@@ -10,20 +14,43 @@ public class Weather {
             "q=%s&" +
             "appid=%s";
     public static String getWeather(String city) throws IOException {
-        String requestUrl = String.format(URL_CURRENT,city,API_KEY);
-        URL url = new URL(requestUrl);
-        URLConnection connection = url.openConnection();
-        InputStream inputStream = connection.getInputStream();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        StringBuffer buffer = new StringBuffer();
-        bufferedReader.lines().forEach(line->buffer.append(line));
+        // forming a query line (I'll set the city and the token)
+        String requesUrl = String.format(URL_CURRENT, city, API_KEY);
 
-
-
+        URL url = new URL(requesUrl);
+        // I'm doing a request and I'm getting a response
+        URLConnection conn = url.openConnection();
         System.out.println("Connection done!");
-        System.out.println(buffer.toString());
-        return buffer.toString();
+
+        // extract data from the answer
+        InputStream is = conn.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader reader = new BufferedReader(isr);
+
+        StringBuffer buffer = new StringBuffer();
+        // response lines in the text buffer
+        reader.lines().forEach(line -> buffer.append(line));
+        System.out.println(buffer);
+
+        String result = buffer.toString();
+        // deserialization from JSON-string to object
+        JSONObject json = new JSONObject(result);
+
+        Map<String, Object> jsonMap = json.toMap();
+
+        Map<String, Object> mainMap = (Map<String, Object>) jsonMap.get("main");
+
+        int temp;
+        if (mainMap.get("temp") instanceof BigDecimal) {
+            temp = ((BigDecimal) mainMap.get("temp")).intValue();
+        } else {
+            temp = (Integer) mainMap.get("temp");
+        }
+
+        city = (String) jsonMap.get("name");
+        result = String.format("Текущая температура в городе %s: %d", city, temp);
+
+        return result;
     }
 
 }

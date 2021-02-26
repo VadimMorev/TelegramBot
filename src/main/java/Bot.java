7f0757321c14f3f99624d2d29a737ba7709e581a
VadimMorev <77121381+VadimMorev@users.noreply.github.com>
2021-02-27 00:39:42 +0200

    Changed Weather class, add temperature, wind speed and weather description to answer string. Add /help command to Bot class.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,6 +4,7 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Bot extends TelegramLongPollingBot {
@@ -19,21 +20,27 @@ public class Bot extends TelegramLongPollingBot {
         if(update.hasMessage()&&update.getMessage().hasText()){
             Message message = update.getMessage();
             String text = message.getText();
+
             System.out.println(text);
             SendMessage answer = new SendMessage();
-            answer.setText("Your message was:"+text);
+
             answer.setChatId(message.getChatId().toString());
-            try {
-                answer.setText(Weather.getWeather(text));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                //sending answer to bot
-                execute(answer);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+            if (text.equals("/help")){
+                answer.setText("Введите название вашего города. А в ответ бот пришлёт вам погоду:)");
+            }else {
+                try {
+                    answer.setText(Weather.getWeather(text));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    answer.setText("Не найден город по вашему запросу!");
+                }}
+                try {
+                    //sending answer to bot
+                    execute(answer);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+
         }
     }
 }
--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -4,15 +4,18 @@ import java.io.*;
 import java.math.BigDecimal;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Weather {
-    private static final String API_KEY="d5ddc3bdb813a23fe8fa89edbac1cca7";
+    private static final String API_KEY = "d5ddc3bdb813a23fe8fa89edbac1cca7";
     private static final String URL_CURRENT = "http://api.openweathermap.org/data/2.5/weather?" +
             "lang=ru&" +
             "units=metric&" +
             "q=%s&" +
             "appid=%s";
+
     public static String getWeather(String city) throws IOException {
         // forming a query line (I'll set the city and the token)
         String requesUrl = String.format(URL_CURRENT, city, API_KEY);
@@ -39,17 +42,38 @@ public class Weather {
         Map<String, Object> jsonMap = json.toMap();
 
         Map<String, Object> mainMap = (Map<String, Object>) jsonMap.get("main");
-
+        String tempString = "";
         int temp;
-        if (mainMap.get("temp") instanceof BigDecimal) {
-            temp = ((BigDecimal) mainMap.get("temp")).intValue();
+
+        if (mainMap.get("temp") instanceof Double) {
+            temp = ((Double) mainMap.get("temp")).intValue();
         } else {
             temp = (Integer) mainMap.get("temp");
         }
+        if (temp > 0) {
+            tempString = "+" + temp;
+        } else {
+            tempString = temp + "";
+        }
+        Map<String,Object> windMap= (Map<String, Object>) jsonMap.get("wind");
+        double windSpeed;
+        if(windMap.get("speed")instanceof Integer){
+            windSpeed=((Integer) windMap.get("speed")).doubleValue();
+        }else {
+            windSpeed = (Double) windMap.get("speed");
+        }
+        ArrayList<Object> arrayList= (ArrayList<Object>) jsonMap.get("weather");
 
+
+        Map<String,Object>weatherMap= (Map<String, Object>) arrayList.get(0);
+        String weatherDescription = weatherMap.get("description").toString();
+        weatherDescription=Character.toUpperCase(weatherDescription.charAt(0))+weatherDescription.substring(1);
+
+//        Map<String,Object>weatherMap= (Map<String, Object>) jsonMap.get("weather");
+//        String weatherDescription=weatherMap.get("description").toString();
         city = (String) jsonMap.get("name");
-        result = String.format("Текущая температура в городе %s: %d", city, temp);
-
+        result = String.format("Текущая температура в городе %s: %s °С. %s. Скорость ветра: %.2f м/с", city, tempString, weatherDescription,windSpeed);
+        System.out.println(result);
         return result;
     }
 
